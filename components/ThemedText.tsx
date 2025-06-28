9c67f86ce93f835fb3842a58837979465b8eb8c7
Michel Gutner <michel.gutner@sanar.com>
2025-06-27 21:05:41 -0300

    build: fix eslint

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link' | 'button';
 };
 
 export function ThemedText({
@@ -22,6 +22,7 @@ export function ThemedText({
       style={[
         { color },
         type === 'default' ? styles.default : undefined,
+        type === 'button' ? styles.button : undefined,
         type === 'title' ? styles.title : undefined,
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
@@ -56,5 +57,11 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
+  },
+  button: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: '600',
+    color: '#fff',
   },
 });
